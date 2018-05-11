1821638968fffedcda08ea1c90a9eaa6e0d584e8
Mikael Simberg <mikael.simberg@iki.fi>
2018-05-11 10:10:31 +0200

    Fix setup-exercises.sh

--- setup-exercises.sh
+++ setup-exercises.sh
@@ -2,4 +2,4 @@
 #cmake -DCMAKE_PREFIX_PATH=/path/to/hpx/root ..
 
 # HPX_DIR needs full path to FindHPX.cmake
-#cmake -DCMAKE_PREFIX_PATH=/path/to/hpx/root/lib/cmake/ ..
+#cmake -DHPX_DIR=/path/to/hpx/root/lib/cmake/HPX ..
