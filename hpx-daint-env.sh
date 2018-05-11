965926007d3f530593f9acd4985fddc43b4225bc
Mikael Simberg <mikael.simberg@iki.fi>
2018-05-11 10:06:51 +0200

    Add separate file with daint environment

---
+++ hpx-daint-env.sh
@@ -0,0 +1,11 @@
+export CRAYPE_LINK_TYPE=dynamic
+module load daint-mc
+module switch PrgEnv-cray PrgEnv-gnu
+module load Boost
+module load hwloc
+
+export CXX=`which CC`
+export CXXFLAGS="-fPIC"
+export TCMALLOC_ROOT=/apps/daint/UES/simbergm/packages/gperftools/2.6.3
+
+export LIBPYTHON_PATH=/usr/lib64/libpython2.7.so.1.0
