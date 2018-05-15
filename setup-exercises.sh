31ba7e7f16c9a1fa63c2de298636fc9a816a8381
Mikael Simberg <mikael.simberg@iki.fi>
2018-05-15 13:39:22 +0200

    Set HPX path in setup-exercises.sh

--- setup-exercises.sh
+++ setup-exercises.sh
@@ -1,5 +1,5 @@
 # CMAKE_PREFIX_PATH looks in subfolders
-#cmake -DCMAKE_PREFIX_PATH=/path/to/hpx/root ..
+#cmake -DCMAKE_PREFIX_PATH=/apps/daint/UES/simbergm/packages/hpx/1.1.0 ..
 
 # HPX_DIR needs full path to FindHPX.cmake
 #cmake -DHPX_DIR=/path/to/hpx/root/lib/cmake/HPX ..
